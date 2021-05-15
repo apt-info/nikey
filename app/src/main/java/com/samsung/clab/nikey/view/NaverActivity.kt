a440f116595bb97ab49de82e5d767958baf7c89c
inasie <inasie@naver.com>
2021-05-15 16:32:10 +0900

    fake naver

--- app/src/main/java/com/samsung/clab/nikey/view/NaverActivity.kt
+++ app/src/main/java/com/samsung/clab/nikey/view/NaverActivity.kt
@@ -1,13 +1,10 @@
 package com.samsung.clab.nikey.view
 
 import android.os.Bundle
-import android.util.Log
-import android.view.KeyEvent
-import android.view.View
-import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.samsung.clab.nikey.R
 import com.samsung.clab.nikey.databinding.ActivityNaverBinding
 import com.samsung.clab.nikey.viewModel.NaverViewModel
@@ -18,22 +15,9 @@ class NaverActivity : AppCompatActivity() {
         var binding: ActivityNaverBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_naver)
         binding.viewModel = ViewModelProvider(this)[NaverViewModel::class.java]
-        initWebView(binding.root)
-    }
 
-    private fun initWebView(view: View) {
-        val webView = view.findViewById<WebView>(R.id.webView)
-        webView.loadUrl("https://m.naver.com")
-    }
-
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        return super.onKeyDown(keyCode, event)
-        Log.i(">>>> TEST", "keyCode: $keyCode")
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
-        return super.onKeyUp(keyCode, event)
-        Log.i(">>>> TEST", "keyCode: $keyCode")
-
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        binding.recyclerView.layoutManager = layoutManager
     }
 }
--- app/src/main/java/com/samsung/clab/nikey/viewModel/KatalkViewModel.kt
+++ app/src/main/java/com/samsung/clab/nikey/viewModel/KatalkViewModel.kt
@@ -1,9 +1,10 @@
 package com.samsung.clab.nikey.viewModel
 
+import android.os.Build
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel
@@ -17,10 +18,20 @@ class KatalkViewModel : ViewModel() {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         }
 
+        @RequiresApi(Build.VERSION_CODES.N)
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            if (itemsMap.containsKey(p0.toString())) {
-                updateItems(itemsMap[p0.toString()]!!)
+            val text = p0.toString()
+            var viewModels: Array<ItemViewModel>? = null
+
+            itemsMap.forEach { (key, items) ->
+                if (text.contains(key)) {
+                    viewModels = items
+                    return@forEach
+                }
+            }
+            if (viewModels != null) {
                 itemsVisible.set(View.VISIBLE)
+                updateItems(viewModels!!)
             } else {
                 itemsVisible.set(View.GONE)
             }
@@ -46,11 +57,13 @@ class KatalkViewModel : ViewModel() {
             ItemViewModel(R.drawable.ic_news, "여행 뉴스", "코로나로 여행객 어쩌구 저쩌구"),
             ItemViewModel(R.drawable.ic_naver, "여행 블로그", "남해 여행 어디까지 가봤니"),
             ItemViewModel(R.drawable.ic_instagram, "남해 카페", "#아난티,#여행,#인생카페"),
-            ItemViewModel(R.drawable.ic_facebook, "페북", "페북 여행 콘텐츠"))
+            ItemViewModel(R.drawable.ic_facebook, "페북", "페북 여행 콘텐츠")
+        )
         itemsMap["휴가"] = arrayOf(
             ItemViewModel(R.drawable.ic_instagram, "남해 여행", "#아난티,#여행,#플렉스"),
             ItemViewModel(R.drawable.ic_naver, "블로그", "전주 한옥마을 휴가 리얼후기"),
             ItemViewModel(R.drawable.ic_round_calendar_today_24, "일정", "하계휴가 7.12~7.15"),
-            ItemViewModel(R.drawable.ic_baseline_image_24, "사진", "갤러리에서 #여행 사진보기"))
+            ItemViewModel(R.drawable.ic_gallery, "사진", "갤러리에서 #여행 사진보기")
+        )
     }
 }
